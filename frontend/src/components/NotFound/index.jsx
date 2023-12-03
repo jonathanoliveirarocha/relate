43d1888be6784ba2b2de578503a387decee2b915
Jonathan de Oliveira Rocha <jonathandeoliveirarocha2002@gmail.com>
2023-12-02 23:49:53 -0300

    Wrong Word

--- frontend/src/components/NotFound/index.jsx
+++ frontend/src/components/NotFound/index.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
       <div className="min-h-[100vb] flex items-center justify-center">
         <div className="">
           <h1 className="text-red-400 font-bold text-3xl ml-4">Erro 404</h1>
-          <p>Página não econtrada</p>
+          <p>Página não encontrada</p>
         </div>
       </div>
     </>
