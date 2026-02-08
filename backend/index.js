6c59c6c7017383a33fabdda41327c68f68c86936
jonathanoliveirarocha <jonathandeoliveirarocha2002@gmail.com>
2026-02-08 02:18:41 -0300

    Add AUTHORIZED_DOMAINS to .env.example and update CORS configuration in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("./src/config/db"); 
-const authRoutes = require("./src/routes/auth"); 
-const articleRoutes = require("./src/routes/article"); 
+require("./src/config/db");
+const authRoutes = require("./src/routes/auth");
+const articleRoutes = require("./src/routes/article");
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors(
+    process.env.AUTHORIZED_DOMAINS.split(",").map((domain) => domain.trim()),
+  ),
+);
 
 app.use("/auth", authRoutes);
 app.use("/articles", articleRoutes);
